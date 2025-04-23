4758ba6c4ae7179f4edeef8a1330c4a37729c701
Ray Carpenter <raymondcarpenter1@gmail.com>
2025-04-23 15:06:42 -0400

    Almost draft night

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from config import FILTERED_STATS_PATH, COMBINE_STATS_PATH
 def main(viz_type="comparison"):
     stats_df = load_data(FILTERED_STATS_PATH, COMBINE_STATS_PATH)
     
-    input_player = "Jalon Walker"
+    input_player = "Tetairoa McMillan"
     
     processor = DataProcessor(stats_df)
     processor.process(input_player)
